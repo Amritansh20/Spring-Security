43e34172c2080a3be39e09d5f65d1a6e7b7ea405
Amritansh20 <soni.amritansh22@gmail.com>
2023-12-01 22:24:36 +0530

    spring security

---
+++ demo/src/main/java/org/securitydemo/demo/MyUserRepository.java
@@ -0,0 +1,8 @@
+package org.securitydemo.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MyUserRepository extends JpaRepository<MyUser, Integer> {
+
+    MyUser findByEmail(String username);
+}
